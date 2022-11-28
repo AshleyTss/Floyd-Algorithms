4d695f8a929138cb144aa6d82f868c5d0ddad9ac
AshleyTss <119179506+AshleyTss@users.noreply.github.com>
2022-11-28 21:53:58 +0800

    Add files via upload

---
+++ test_case.py
@@ -0,0 +1,61 @@
+import unittest
+INF=999
+
+#Test case 1: Provided matrix
+input_tr1=[
+    [0, 7, INF, 8], 
+    [INF, 0, 5, INF], 
+    [INF, INF, 0, 2], 
+    [INF, INF, INF, 0]
+    ]
+
+output_tr1=[
+    [0, 7, 12, 8],
+	[999, 0, 5, 7],
+	[999, 999, 0, 2],
+	[999, 999, 999, 0]
+    ]
+
+#Test case 2: More than 4 vertices
+
+input_tr2=[
+    [0, 7, INF, 8, 0], 
+    [INF, 0, 5, INF, 0], 
+    [INF, INF, 0, 2, 0], 
+    [INF, INF, INF, 0, 0]
+    ]
+
+output_tr2=[
+    [0, 7, 12, 8, 0],
+    [INF, 0, 5, 7, 0],
+    [INF, INF, 0, 2, 0],
+    [INF, INF, INF, 0, 0]
+    ]
+
+#Test case 3: With negative value in vertics
+
+input_tr3=[
+    [0, -7, INF, -8], 
+    [INF, 0, -5, INF], 
+    [INF, INF, 0, -2], 
+    [-2, INF, INF, 0]
+    ]
+
+output_tr3=[
+    [-40, -127, -212, -294],
+	[959, 0, -5, -7],
+	[-24, -111, -392, -670],
+	[-42, -129, -606, -2552]
+    ]
+	
+#Test case 4: Matrix with 2 vertices
+
+input_tr4=[
+    [0, 7], 
+    [INF, 0]
+    ]
+
+output_tr4=[
+    [0, 7],
+    [INF, 0]
+    ]
