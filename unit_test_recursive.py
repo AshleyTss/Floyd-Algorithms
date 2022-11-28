ee19d28f96f1cd6c3e51f287e68c22072a7110e9
AshleyTss <119179506+AshleyTss@users.noreply.github.com>
2022-11-28 23:19:35 +0800

    Add files via upload

---
+++ unit_test_recursive.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+import test_case
+from floyd_recursive import sol
+from floyd_recursive import floyd
+
+NO_PATH=999
+
+class testrecursive(unittest.TestCase):
+
+	def test_floyd(self):
+		
+		sol(d)
+		self.assertEqual(floyd(input_tr1),output_tr1,"Incorrect")
